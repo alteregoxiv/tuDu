9b246212f35f8c85d0bb75b03a5140014d01d874
alteregoxiv1 <prasad.biswasxiv@gmail.com>
2020-11-04 18:14:02 +0000

    Server Created

--- src/main/java/org/tudu/app/App.java
+++ src/main/java/org/tudu/app/App.java
@@ -4,10 +4,23 @@ package org.tudu.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+ 
+ import java.io.IOException;
+ import java.net.InetSocketAddress;
+ 
+ import com.sun.net.httpserver.HttpServer;
+ 
+public class App {
+    
+    private final static Integer PORT = Integer.valueOf(System.getenv("PORT"));
+    
+    public static void main( String[] args ) throws IOException {
+    
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT) , 0);
+        
+        server.setExecutor(null);
+        server.start();
+        System.out.println("[ Server ] listening at port : " + PORT);
+        
     }
 }
