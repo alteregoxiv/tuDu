1ca91386cf65ca01253c6bb877dd05abc8a5c2a7
Gourish Sadhu <sadhu5gourish@outlook.com>
2020-11-04 19:21:48 +0530

    Tried reading file

--- setenv.ps1
+++ setenv.ps1
@@ -1,2 +1,3 @@
+$Env:DIR="$(Get-Location)"
 $Env:CLASSPATH += ";$(Get-Location)\target\classes"
 $Env:PORT = 3000
--- src/main/java/org/tudu/app/App.java
+++ src/main/java/org/tudu/app/App.java
@@ -7,20 +7,42 @@ package org.tudu.app;
  
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
  
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
  
 public class App {
     
     private final static Integer PORT = Integer.valueOf(System.getenv("PORT"));
+    private final static String BASEDIR = System.getenv("DIR");
     
     public static void main( String[] args ) throws IOException {
     
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT) , 0);
+
+        server.createContext("/", new HttpHandler() {
+        	@Override
+        	public void handle(HttpExchange t) throws IOException, FileNotFoundException {
+        		File file = new File(System.getenv("DIR") + "/todo.json");
+        		FileReader fr = new FileReader(file);
+        		BufferedReader in = new BufferedReader(fr);
+        		String data = new String();
+        		String s;
+        		while ((s = in.readLine()) != null)
+        			data += s;
+        		System.out.println(data);
+        	}
+        });
         
         server.setExecutor(null);
         server.start();
-        System.out.println("[ Server ] listening at port : " + PORT);
+        System.out.println("[ Server  ☑✔✅] listening at port : " + PORT);
         
     }
 }
